e6703b56fd07143b4961667e7113d7f9ba71e788
kvega005 <kvegagonzalez@ufl.edu>
2023-04-05 16:47:59 -0400

    unit tests for counter

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,34 @@
 // import necessary react testing library helpers here
+import { render, screen } from '@testing-library/react';
 // import the Counter component here
+import Counter from '../components/Counter'
+import userEvent from '@testing-library/user-event';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter/>)
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText("Counter"))
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(0))
 });
 
-test('clicking + increments the count', () => {
+test('clicking + increments the count', async () => {
   // Complete the unit test below based on the objective in the line above
+  await userEvent.click(screen.getByText('+'))
+
+  expect(screen.getByText('1'))
 });
 
-test('clicking - decrements the count', () => {
+test('clicking - decrements the count', async () => {
   // Complete the unit test below based on the objective in the line above
+  await userEvent.click(screen.getByText('-'))
+  
+  expect(screen.getByText('-1'))
 });
